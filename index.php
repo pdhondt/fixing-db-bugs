7122d72ee2e9f49ca3bd4cc88d12dd1dcc0ecf84
Peter <dhondt.peter@gmail.com>
2021-03-12 10:24:31 +0100

    fix error line 78 id -> user.id

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ elseif(isset($_POST['delete'])) {
 }
 
 //@todo Invalid query?
-$handle = $pdo->prepare('SELECT user.id, concat_ws(firstname, lastname, " ") AS name, sport.sport FROM user LEFT JOIN sport ON user.id = sport.user_id where year = :year order by sport');
+$handle = $pdo->prepare('SELECT user.id, concat_ws(firstname, lastname, " ") AS name, sport FROM user LEFT JOIN sport ON user.id = sport.user_id where year = :year order by sport');
 $handle->bindValue(':year', date('Y'));
 $handle->execute();
 $users = $handle->fetchAll();
